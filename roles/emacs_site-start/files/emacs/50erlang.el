4c43b2b7ec9f12e4e2522ce811c6064cec96d9c6
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2015-10-11 14:24:57 -0700

    erlang stuff and sr-speedbar

--- roles/emacs_site-start/files/emacs/50erlang.el
+++ roles/emacs_site-start/files/emacs/50erlang.el
@@ -1,3 +1,16 @@
 (unless (package-installed-p 'erlang)
   (package-install 'erlang))
 (require 'erlang-start)
+
+(defun my/erlang-mode-hook nil
+  (progn
+    (turn-on-auto-fill)
+    (subword-mode)))
+
+(add-hook 'erlang-mode-hook 'my/erlang-mode-hook)
+
+(defun my/whitespace-hook ()
+  (setq show-trailing-whitespace 't))
+
+(add-hook 'prog-mode-hook 'my/whitespace-hook)
+(add-hook 'erlang-mode-hook 'my/whitespace-hook)
