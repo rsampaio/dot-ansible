99e4e3272ececdd3a8f906a001f3d4673d9f11bb
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2015-10-27 23:41:21 -0700

    org-capture keybindings

---
+++ roles/emacs_site-start/files/emacs/99rsampaio-org-mode.el
@@ -0,0 +1,37 @@
+(require 'org)
+
+(setq org-directory "~/Dropbox/org")
+(setq org-agenda-files (list org-directory))
+(add-to-list 'org-agenda-files org-journal-dir)
+(setq org-agenda-file-regexp "\\`[^.].*\\.org'\\|[0-9]+")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq
+ org-capture-templates
+ '(
+   ("T" "Personal Todo" entry (file+headline (concat org-directory "/mytodo.org") "Tasks")
+    "* TODO %?\n  %u")
+   ("t" "Work Todo" entry (file+headline (concat org-directory "/worktodo.org") "Tasks")
+    "* TODO %?\n  %u")
+   ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+    "* %?\nEntered on %U\n  %i")
+   ("r" "Random" entry (file+datetree (concat org-directory "/random.org"))
+    "* %?\nEntered on %U\n  %i")
+   )
+ )
+
+(define-key global-map "\C-ct"
+  (lambda () (interactive) (org-capture nil "t")))
+
+(define-key global-map "\C-cT"
+  (lambda () (interactive) (org-capture nil "T")))
+
+(define-key global-map "\C-cr"
+  (lambda () (interactive) (org-capture nil "r")))
+
+(define-key global-map "\C-cj"
+  (lambda () (interactive) (org-capture nil "j")))
