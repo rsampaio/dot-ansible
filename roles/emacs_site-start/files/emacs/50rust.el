d1b6b095d2b75552516a7f52f8e4aba7317d55af
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2017-02-22 20:11:35 -0800

    remove rustfmt

--- roles/emacs_site-start/files/emacs/50rust.el
+++ roles/emacs_site-start/files/emacs/50rust.el
@@ -1,7 +1,2 @@
 (unless (package-installed-p 'rust-mode)
   (package-install 'rust-mode))
-
-(unless (package-installed-p 'rustfmt)
-  (package-install 'rustfmt))
-
-(add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
