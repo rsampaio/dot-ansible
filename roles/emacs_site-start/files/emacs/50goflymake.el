b42ae22d4727c08816603ea069c9cef0db2f6492
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2017-08-11 10:05:17 -0700

    organize go stuff ugh

--- roles/emacs_site-start/files/emacs/50goflymake.el
+++
@@ -1,6 +0,0 @@
-(unless (package-installed-p 'flymake)
-  (package-install 'flymake)
-  (package-install 'flycheck))
-(add-to-list 'load-path "~/Code/go/src/github.com/dougm/goflymake")    
-(require 'go-flymake)
-(require 'go-flycheck)
--- roles/emacs_site-start/files/emacs/90go.el
+++ roles/emacs_site-start/files/emacs/90go.el
@@ -17,3 +17,9 @@
 
 (ac-config-default)
 (setq gofmt-command "goimports")
+(unless (package-installed-p 'flymake)
+  (package-install 'flymake)
+  (package-install 'flycheck))
+(add-to-list 'load-path "~/Code/go/src/github.com/dougm/goflymake")    
+(require 'go-flymake)
+(require 'go-flycheck)
